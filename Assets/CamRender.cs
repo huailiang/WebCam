1607e22acce3ebf0fd18d627d461dbfef3aa3e05
huailiang.peng <penghuailiang@126.com>
2021-12-26 22:45:08 +0800

    ae & iso

--- Assets/CamRender.cs
+++ Assets/CamRender.cs
@@ -4,15 +4,19 @@ using UnityEngine.UI;
 
 public class CamRender : MonoBehaviour
 {
-    private float slider;
+    public Slider zoomSlider, isoSlider, aeSlider, focusSlider;
     private WebCamTexture webCamTexture;
     private WebCamDevice[] devices;
     private int devID;
     private AndroidJavaObject wrapper;
     public RawImage image;
+    private AndroidJavaClass cameraClass;
 
     IEnumerator Start()
     {
+        var tf = image.GetComponent<RectTransform>();
+        tf.sizeDelta = new Vector2(Screen.width, 1.33f * Screen.width);
+        cameraClass = new AndroidJavaClass("com.yun.webCam.Camera2Test");
         yield return Application.RequestUserAuthorization(UserAuthorization.WebCam);
         if (Application.HasUserAuthorization(UserAuthorization.WebCam))
         {
@@ -21,10 +25,18 @@ public class CamRender : MonoBehaviour
             {
                 devID = 0;
                 Play(devices[0].name);
+#if UNITY_ANDROID && !UNITY_EDITOR
+                NativeInit();
+#endif
             }
         }
+
+        zoomSlider.onValueChanged.AddListener(SetZoom);
+        isoSlider.onValueChanged.AddListener(SetISO);
+        aeSlider.onValueChanged.AddListener(SetAE);
+        focusSlider.onValueChanged.AddListener(SetFocus);
     }
-    
+
     private void OnGUI()
     {
         int len = devices?.Length ?? 0;
@@ -40,31 +52,51 @@ public class CamRender : MonoBehaviour
                 }
             }
         }
-        if (GUI.Button(new Rect(240, 0, 120, 80), "ZoomInit"))
-        {
-            var pl_class = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-            var currentActivity = pl_class.GetStatic<AndroidJavaObject>("currentActivity");
-            var player = currentActivity.Get<AndroidJavaObject>("mUnityPlayer");
-            wrapper = player.Get<AndroidJavaObject>("p"); // Camera2Wrapper
-            var b = wrapper.Get<AndroidJavaObject>("b");
-            var builder = b.Get<AndroidJavaObject>("t"); // CaptureRequest.Builder
-            var camMgr = b.GetStatic<AndroidJavaObject>("b"); // CameraManager
-            if (camMgr != null && builder != null)
-            {
-                pl_class = new AndroidJavaClass("com.yun.webCam.Camera2Test");
-                pl_class.CallStatic("SetMgrAndCharacteristics", camMgr, builder, devID.ToString());
-            }
-        }
-        if (GUI.Button(new Rect(240, 100, 120, 80), "ZoomSet"))
-        {
-            webCamTexture.Pause();
-            var pl_class = new AndroidJavaClass("com.yun.webCam.Camera2Test");
-            pl_class.CallStatic("SetZoom", slider);
-            webCamTexture.Play();
-        } 
-        slider = GUI.HorizontalSlider(new Rect(240, 320, 520, 80), slider, 0.1f, 8.0f);
     }
-    
+
+    private void NativeInit()
+    {
+        var pl_class = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+        var currentActivity = pl_class.GetStatic<AndroidJavaObject>("currentActivity");
+        var player = currentActivity.Get<AndroidJavaObject>("mUnityPlayer");
+        wrapper = player.Get<AndroidJavaObject>("p"); // Camera2Wrapper
+        var b = wrapper.Get<AndroidJavaObject>("b");
+        var builder = b.Get<AndroidJavaObject>("t"); // CaptureRequest.Builder
+        var camMgr = b.GetStatic<AndroidJavaObject>("b"); // CameraManager
+        if (camMgr != null && builder != null)
+            cameraClass.CallStatic("SetMgrAndCharacteristics", camMgr, builder, devID.ToString());
+    }
+
+    private void SetZoom(float v)
+    {
+        // webCamTexture.Pause();
+        // cameraClass.CallStatic("SetZoom", 8.0f * v);
+        // webCamTexture.Play();
+        SetCameraCharacteristics("SetZoom", v);
+    }
+
+    private void SetISO(float v)
+    {
+        SetCameraCharacteristics("SetISO", v);
+    }
+
+    private void SetAE(float v)
+    {
+        SetCameraCharacteristics("SetAe", v);
+    }
+
+    private void SetFocus(float v)
+    {
+        SetCameraCharacteristics("SetFocusDistance", v);
+    }
+
+    private void SetCameraCharacteristics(string key, float v)
+    {
+        webCamTexture.Pause();
+        cameraClass.CallStatic(key, (int) (100 * v));
+        webCamTexture.Play();
+    }
+
     private void Play(string devicename)
     {
         webCamTexture = new WebCamTexture(devicename, 480, 640, 30)
--- android/WebCam/src/main/java/com/yun/webCam/Camera2Test.java
+++ android/WebCam/src/main/java/com/yun/webCam/Camera2Test.java
@@ -1,28 +1,23 @@
 package com.yun.webCam;
 
 import android.graphics.Rect;
+import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
 import android.util.Log;
+import android.util.Range;
+
+import java.util.PrimitiveIterator;
 
 public class Camera2Test
 {
     static String TAG = "Unity";
-
-    private static void Print2(int a, int b)
-    {
-        Log.d(TAG, "Print2 *********** " + a + " b:" + b);
-    }
-
     private static CameraCharacteristics mCameraCharacteristics;
     private static CaptureRequest.Builder mPreviewRequestBuilder;
-    private static CameraManager mCameraManager;
-
     private static final float DEFAULT_ZOOM_FACTOR = 1.0f;
-    private static Rect mCropRegion = new Rect();
+    private static final Rect mCropRegion = new Rect();
     public static float maxZoom;
-    private static float mCurrentZoomFactor = DEFAULT_ZOOM_FACTOR;
     private static Rect mSensorSize;
     public static boolean hasSupport;
 
@@ -30,18 +25,17 @@ public class Camera2Test
     public static void SetMgrAndCharacteristics(CameraManager mgr, CaptureRequest.Builder b, String name)
     {
         Log.d(TAG, "SetMgrAndCharacteristics: " + (b == null));
-        mCameraManager = mgr;
         mPreviewRequestBuilder = b;
         try {
-            mCameraCharacteristics = mCameraManager.getCameraCharacteristics(name);
-            Zoom(mCameraCharacteristics);
+            mCameraCharacteristics = mgr.getCameraCharacteristics(name);
+            GetZoom(mCameraCharacteristics);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
 
-    private static void Zoom(final CameraCharacteristics characteristics)
+    private static void GetZoom(final CameraCharacteristics characteristics)
     {
         mSensorSize = characteristics.get(CameraCharacteristics.SENSOR_INFO_ACTIVE_ARRAY_SIZE);
         if (mSensorSize == null) {
@@ -57,23 +51,63 @@ public class Camera2Test
         Log.d(TAG, "maxZoom: " + maxZoom + " default:" + DEFAULT_ZOOM_FACTOR + "  hasSupport: " + hasSupport);
     }
 
+    /**
+     * 获取曝光补偿
+     *
+     * @param i 0-100 百分比
+     */
+    private static void SetAe(int i)
+    {
+        Range<Integer> integerRange = mCameraCharacteristics.get(CameraCharacteristics.CONTROL_AE_COMPENSATION_RANGE);
+        int min = integerRange.getLower();
+        int max = integerRange.getUpper();
+        int all = max - min;
+        int time = 100 / all;
+        int ae = ((i / time) - max) > max ? max : Math.max(((i / time) - max), min);
+        Log.d(TAG, "AE: " + ae + " min: " + min + " max: " + max);
+        mPreviewRequestBuilder.set(CaptureRequest.CONTROL_AE_EXPOSURE_COMPENSATION, ae);
+    }
 
-    public static void SetZoom(float newZoom)
+
+    /**
+     * ISO感光灵敏度
+     *
+     * @param i 0-100 百分比
+     */
+    private static void SetISO(int i)
+    {
+        Range<Integer> range = mCameraCharacteristics.get(CameraCharacteristics.SENSOR_INFO_SENSITIVITY_RANGE);
+        int max = range.getUpper();
+        int min = range.getLower();
+        int iso = ((i * (max - min)) / 100 + min);
+        mPreviewRequestBuilder.set(CaptureRequest.SENSOR_SENSITIVITY, iso);
+    }
+
+
+    /**
+     * 设置焦距
+     *
+     * @param i 0-100 百分比
+     */
+    private static void SetFocusDistance(int i)
+    {
+        float minimumLens = mCameraCharacteristics.get(CameraCharacteristics.LENS_INFO_MINIMUM_FOCUS_DISTANCE);
+        float num = (((float) i) * minimumLens / 100);
+        mPreviewRequestBuilder.set(CaptureRequest.LENS_FOCUS_DISTANCE, num);
+    }
+
+    /**
+     * 获取缩放
+     */
+    public static void SetZoom(int zoom)
     {
         if (!hasSupport) return;
-
-        if (newZoom < DEFAULT_ZOOM_FACTOR) newZoom = DEFAULT_ZOOM_FACTOR;
-        if (newZoom > maxZoom) newZoom = maxZoom;
-        mCurrentZoomFactor = newZoom;
+        float mCurrentZoomFactor = (maxZoom - DEFAULT_ZOOM_FACTOR) * (zoom / 100.0f) + DEFAULT_ZOOM_FACTOR;
         final int centerX = mSensorSize.width() / 2;
         final int centerY = mSensorSize.height() / 2;
         final int deltaX = (int) ((0.5f * mSensorSize.width()) / mCurrentZoomFactor);
         final int deltaY = (int) ((0.5f * mSensorSize.height()) / mCurrentZoomFactor);
-        mCropRegion.set(centerX - deltaX,
-                centerY - deltaY,
-                centerX + deltaX,
-                centerY + deltaY);
-
+        mCropRegion.set(centerX - deltaX, centerY - deltaY, centerX + deltaX, centerY + deltaY);
         mPreviewRequestBuilder.set(CaptureRequest.SCALER_CROP_REGION, mCropRegion);
         Log.d(TAG, "SetZoom: " + mCurrentZoomFactor + "  rect: " + mCropRegion);
     }
